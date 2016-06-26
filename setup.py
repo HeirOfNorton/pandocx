1b9ed2f3c1002573a5a8cf02345e8bb9d3a1cad7
Jason M. Seeley <jamiseeley@yahoo.com>
2016-06-25 19:56:57 -0500

    Base structure and scaffolding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pandocx',
+        version='0.1',
+        description='Wrapper and tools for working with Pandoc and Microsoft Word DOCX files.',
+        url='http://github.com/HeirOfNorton/pandocx.git',
+        author='Jason M. Seeley',
+        license='MIT',
+        packages=['pandocx'],
+        zip_safe=False)
+
